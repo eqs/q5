fda92db8816005cedeee078c089635b5b09d36a7
eqs <murashige.satoshi.mi1@gmail.com>
2022-04-26 19:32:45 +0900

    Add example numpy function

--- src/math_utils.rs
+++ src/math_utils.rs
@@ -1,11 +1,23 @@
 use pyo3::prelude::*;
+use ndarray::{ArrayD, ArrayViewD, ArrayViewMutD};
+use numpy::{Element, IntoPyArray, PyArrayDyn};
 
 #[pyfunction]
 fn some_func() -> u32{
     42
 }
 
+#[pyfunction]
+fn add_numpy(py: Python, x: &PyArrayDyn<f64>, y: &PyArrayDyn<f64>) -> PyResult<Py<PyArrayDyn<f64>>> {
+    unsafe{
+        let x = x.as_array();
+        let y = y.as_array();
+        Ok((&x + &y).into_pyarray(py).to_owned())
+    }
+}
+
 pub fn add_math_functions(m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(some_func, m)?)?;
+    m.add_function(wrap_pyfunction!(add_numpy, m)?)?;
     Ok(())
 }
