03a320f9cbf06ac59255e09402d23f9c55865fa0
eqs <murashige.satoshi.mi1@gmail.com>
2022-09-26 02:45:59 +0900

    Add an example

--- examples/q5_basics/draw_image.py
+++ examples/q5_basics/draw_image.py
@@ -4,7 +4,16 @@ import q5
 
 class App(q5.BaseApp):
     def setup(self):
-        img = np.zeros((200, 200, 3))
+        img = np.zeros((480, 480, 3))
+
+        ii, jj = np.meshgrid(
+            np.arange(480), np.arange(480)
+        )
+
+        img[ii, jj, 0] = ii / 480 * 255
+        img[ii, jj, 1] = jj / 480 * 255
+        img[ii, jj, 2] = 0.0
+
         self.img = q5.QImage(img)
 
     def update(self):
@@ -12,7 +21,18 @@ class App(q5.BaseApp):
 
     def draw(self):
         q5.background(220)
-        q5.image(self.img, 0.0, 0.0)
+
+        t = q5.frame_count / 60.0 * np.pi * 0.25
+        for k in range(8):
+            p = k / 8 * np.pi * 2.0
+            x = 200.0 * np.cos(p + t)
+            y = 200.0 * np.sin(p + t)
+            q5.push_matrix()
+            q5.translate(x, y)
+            q5.scale(0.15)
+            q5.rotate(p + t)
+            q5.image(self.img, 0.0, 0.0)
+            q5.pop_matrix()
 
 
 if __name__ == '__main__':
